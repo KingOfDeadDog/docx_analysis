17af277d20f0b3a12ed4eecdb1a11c2e0eed8d6c
KingOfDeadDog <kingofdeaddog@gmail.com>
2017-09-09 17:26:25 +0100

    added folder for testing as well as first placeholder testing module

---
+++ tests/test_main.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class MainTestCase(unittest.TestCase):
+    """Tests for main module"""
+
+    def test_is_analysis_correct(self):
+        """Will compare analysis results of some knows documents where I know
+        what the results should be and will check if the results match"""
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
